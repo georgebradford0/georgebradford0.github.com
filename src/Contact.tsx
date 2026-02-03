0dff10448b539192109c2a9b17ac0191fb0a70ee
George Bradford <georgebradford0@proton.me>
2026-02-02 16:44:48 -0800

    Contact update

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -3,14 +3,7 @@ function Contact() {
     <div className="container">
       <main className="contact-page">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris.
-        </p>
-        <p>
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          All inquiries can be sent to georgebradford0@proton.me.
         </p>
       </main>
     </div>

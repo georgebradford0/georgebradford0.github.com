59ca1cf422bf1b1df37b49ffac1f49c08ee1a21e
George Bradford <georgebradford0@proton.me>
2026-06-29 17:18:53 -0700

    Remove link

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,7 @@
-import { Link } from 'react-router-dom'
-
 function Home() {
   return (
     <div className="container">
       <nav className="article-links">
-        <Link to="https://github.com/georgebradford0/okto.git" className="article-link">
-          Okto - A mobile LLM agent management platform
-        </Link>
       </nav>
     </div>
   )
